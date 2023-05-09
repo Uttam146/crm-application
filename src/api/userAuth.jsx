b2a4821299794a28f39fd3d59becb186c42533c6
uttam rawat <uttamrawat146>
2023-05-09 10:43:54 +0530

    Login and SignUp work done

--- src/api/userAuth.jsx
+++ src/api/userAuth.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+if (process.env.NETLIFY === 'true') {
+    const BASE_URL = 'https://crm-backend-iukg.onrender.com';
+} else {
+    const BASE_URL = import.meta.env.VITE_BASE_URL;
+}
 
 
-export async function SignUpForm(data){
-    return axios.post(`${BASE_URL}/crm/api/v1/auth/signup`,data);
-}   
 
-export async function SignInForm(data){
-    return axios.post(`${BASE_URL}/crm/api/v1/auth/signin`,data);
+export async function SignUpForm(data) {
+    return axios.post(`${BASE_URL}/crm/api/v1/auth/signup`, data);
+}
+
+export async function SignInForm(data) {
+    return axios.post(`${BASE_URL}/crm/api/v1/auth/signin`, data);
 }   
